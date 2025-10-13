0beb08ca6e61343b79c945f8a6df28897dcccb6f
srikk <122169588+py-code314@users.noreply.github.com>
2025-10-12 22:34:27 -0400

    feat: Fetch data & display images in `App` component
    - Add filter parameters to api url.
    - Add loading state and error state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,71 @@
-// import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
 
 function App() {
-  
+  // const [currentScore, setCurrentScore] = useState(0)
+  // const [bestScore, setBestScore] = useState(0)
+  const [data, setData] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
 
+  // console.log(data)
+
+  useEffect(() => {
+    // Ignore flag and setData(null) - for safety and future proofing
+    let ignore = false
+    setData(null)
+
+    async function fetchData() {
+      setIsLoading(true)
+      setIsError(false)
+
+      try {
+        const response = await fetch(
+          'https://rickandmortyapi.com/api/character/[1,2,4,5,47,103,118,242,244,331,372,629]'
+        )
+        // Check response for HTTP errors
+        if (!response.ok) {
+          throw new Error()
+        }
+        const data = await response.json()
+        if (!ignore) {
+          setData(data)
+        }
+      } catch (error) {
+        // Catches network errors
+        console.error('Error fetching data:', error)
+        setIsError(true)
+      }
+
+      setIsLoading(false)
+    }
+
+    fetchData()
+    
+    // Clean-up function
+    return () => {
+      ignore = true
+    }
+  }, [])
+
+  // * Remove all button styles with - all:unset
   return (
-    <div>Test</div>
+    <div>
+      {isLoading && <p>...Loading</p>}
+      {isError && <p>Error</p>}
+      {data &&
+        data.map((character) => (
+          <ul key={character.id}>
+            <li>
+              <button type="button">
+                <img src={character.image} alt="" />
+                <p>{character.name}</p>
+              </button>
+            </li>
+          </ul>
+        ))}
+    </div>
   )
 }
 
