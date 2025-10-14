9d220a7b71167ffa2433e30063da344baddfdd24
srikk <122169588+py-code314@users.noreply.github.com>
2025-10-13 21:49:02 -0400

    style: Add styles to ScoreBoard
    - Add Bungee font.
    - Add custom border style to scores.

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,10 +1,18 @@
 import '../styles/ScoreBoard.css'
 
 const ScoreBoard = ({ currentScore, bestScore }) => {
-  return (<div className='score'>
-    <p className='score__current'>Current Score: {currentScore}</p>
-    <p className='score__best'>Best Score: {bestScore}</p>
-  </div> );
+  return (
+    <div className="score">
+      <p className="score__current">
+        <span className="score__label">Schmeckles collected: </span>
+        <span className="score__number">{currentScore}</span>
+      </p>
+      <p className="score__best">
+        <span className="score__label">Highest dimension jump: </span>
+        <span className="score__number">{bestScore}</span>
+      </p>
+    </div>
+  )
 }
  
 export default ScoreBoard;
