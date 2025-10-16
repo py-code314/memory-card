34360305adcef7746ec56fa66cab82ef1648254d
srikk <122169588+py-code314@users.noreply.github.com>
2025-10-16 14:24:09 -0400

    feat: Add loading animation & error message

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,34 @@ import { ImageList, Image } from './components/ImageList'
 import ScoreBoard from './components/ScoreBoard'
 import ModalDialog from './components/ModalDialog'
 import { shuffleCards } from './utils/randomizeArray'
-
-import './App.css'
 import rickIcon from './assets/images/icon-rick.svg'
 import mortyIcon from './assets/images/icon-morty.svg'
+import './App.css'
 
 function App() {
-  const [currentScore, setCurrentScore] = useState(0)
-  const [bestScore, setBestScore] = useState(0)
   const [data, setData] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
   const [isError, setIsError] = useState(false)
   const [cardIds, setCardIds] = useState([])
+  const [currentScore, setCurrentScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
   const [showModal, setShowModal] = useState(false)
-  // TODO: useReducer for state
 
   // console.log(data)
 
   useEffect(() => {
     // Add ignore flag and setData(null) - for safety and future proofing
     let ignore = false
+    console.log('setData to null')
     setData(null)
 
     async function fetchData() {
+      console.log('isLoading true')
       setIsLoading(true)
       setIsError(false)
 
       try {
+        console.log('fetching data')
         const response = await fetch(
           'https://rickandmortyapi.com/api/character/[1,2,4,5,47,103,118,242,244,331,372,629]'
         )
@@ -39,14 +40,17 @@ function App() {
         }
         const data = await response.json()
         if (!ignore) {
+          // console.log('setData to data')
           setData(data)
+          // console.log('isLoading second time')
+          // setIsLoading(false)
         }
       } catch (error) {
         // Catches network errors
         console.error('Error fetching data:', error)
         setIsError(true)
       }
-
+      // console.log('isLoading false')
       setIsLoading(false)
     }
 
@@ -84,6 +88,7 @@ function App() {
 
   // TODO: Extract header component
   // TODO: Fix title contrast check
+  console.log(isLoading)
   return (
     <>
       <header className="header">
@@ -118,17 +123,15 @@ function App() {
         )}
         <ScoreBoard currentScore={currentScore} bestScore={bestScore} />
         <div className="cards">
-          {isLoading && <p>...Loading</p>}
-          {isError && <p>Error</p>}
           {data && shuffleCards(data)}
-
-          <ImageList>
+          <ImageList isLoading={isLoading} isError={isError}>
             {data &&
               data.map((character) => (
                 <Image
                   key={character.id}
                   character={character}
                   handleClick={handleCardClick}
+                  isLoading={isLoading}
                 />
               ))}
           </ImageList>
--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,22 +1,44 @@
 import '../styles/ImageList.css'
 
-export const ImageList = ({ children }) => {
-  return <ul className="characters">{children}</ul>
+export const ImageList = ({ children, isError, isLoading }) => {
+  return (
+    <>
+      {isError ? (
+        <div className="error">
+          <p className="error__text">
+            <span>Error loading images.</span>{' '}
+            <span>Please try again later...</span>
+          </p>
+        </div>
+      ) : (
+        <ul className="characters">
+          {isLoading && (
+            <div className="loading">
+              <div className="loading__spinner"></div>
+            </div>
+          )}
+          {children}
+        </ul>
+      )}
+    </>
+  )
 }
 
 export const Image = ({ character, handleClick }) => {
   return (
-    <li className="character">
-      <button
-        className="character__btn"
-        type="button"
-        id={character.id}
-        onClick={handleClick}>
-        <div className="character__image-container">
-          <img className="character__image" src={character.image} alt="" />
-        </div>
-        <p className="character__name">{character.name}</p>
-      </button>
-    </li>
+    <>
+      <li className="character">
+        <button
+          className="character__btn"
+          type="button"
+          id={character.id}
+          onClick={handleClick}>
+          <div className="character__image-container">
+            <img className="character__image" src={character.image} alt="" />
+          </div>
+          <p className="character__name">{character.name}</p>
+        </button>
+      </li>
+    </>
   )
 }
