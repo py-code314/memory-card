72eaef2b56d9c2b72fcf2f6721b4409906ca8ae9
srikk <122169588+py-code314@users.noreply.github.com>
2025-10-17 18:29:12 -0400

    fix: Show win message when player completes the game

--- src/components/ModalDialog.jsx
+++ src/components/ModalDialog.jsx
@@ -2,19 +2,24 @@ import '../styles/ModalDialog.css'
 
 /* A modal dialog component that displays a game over message */
 const ModalDialog = ({ currentScore, bestScore, handleClick }) => {
+  const winner = currentScore === 12
   return (
     <>
       <div className="backdrop"></div>
       <div className="dialog">
-        <h2 className='dialog__title'>Game Over!</h2>
-        <p className='dialog__score'>Current Score: {currentScore}</p>
-        <p className='dialog__score'>Best Score: {bestScore}</p>
-        <button className='dialog__btn' type="button" onClick={handleClick}>
+        {winner ? (
+          <h2 className="dialog__title">You won!</h2>
+        ) : (
+          <h2 className="dialog__title">Game Over!</h2>
+        )}
+        <p className="dialog__score">Current Score: {currentScore}</p>
+        <p className="dialog__score">Best Score: {bestScore}</p>
+        <button className="dialog__btn" type="button" onClick={handleClick}>
           Restart
         </button>
       </div>
     </>
   )
 }
- 
-export default ModalDialog;
+
+export default ModalDialog
